69aee3ea822d92292bd002c48ccf939afd18ad8a
visheshkochher <vishesh.kochher@gmail.com>
2017-11-07 17:52:04 +0100

    added live_data_stream

---
+++ scripts/live_data_stream.py
@@ -0,0 +1,9 @@
+import tide_data as tide
+import wave_data as wave
+
+while True:
+    try:
+        tide.get_tide_data()
+        wave.get_wave_data()
+    except:
+        print("Error in data refresh")
