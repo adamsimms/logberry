8cc05a4b3a072e9c54d6fd2e60bf318e380aab31
Adam Simms <adam@trello.com>
2018-03-19 09:43:46 -0400

    Updated defaults.

--- scripts/data_input.py
+++ scripts/data_input.py
@@ -1,4 +1,4 @@
-lowest_tide = 10 #cmToStep(eval(input("Enter height of lowest tide from ground in cm (test = 2): ")))
-tide_range = 10 #cmToStep(eval(input("Enter height between lowest tide and highest tide in cm (test = 30): ")))
-multiplier = 1 #eval(input("Enter wave multiplier: "))
-speed_multiplier = 1 #min(eval(input("Enter speed multiplier  (0.8, 1, max = 2): ")),2)
+lowest_tide = 0 #cmToStep(eval(input("Enter height of lowest tide from ground in cm (test = 2): ")))
+tide_range = 250 #cmToStep(eval(input("Enter height between lowest tide and highest tide in cm (test = 30): ")))
+multiplier = 8 #eval(input("Enter wave multiplier: "))
+speed_multiplier = 1.6 #min(eval(input("Enter speed multiplier  (0.8, 1, max = 2): ")),2)
