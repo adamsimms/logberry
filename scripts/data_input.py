3432094c7e8fbff08d5e41ac9f3307707e35d642
Adam Simms <adamsimms@gmail.com>
2018-04-25 18:52:14 -0400

    Updated values.

--- scripts/data_input.py
+++ scripts/data_input.py
@@ -1,4 +1,4 @@
-lowest_tide = 100 #cmToStep(eval(input("Enter height of lowest tide from ground in cm (test = 2): ")))
-tide_range = 200 #cmToStep(eval(input("Enter height between lowest tide and highest tide in cm (test = 30): ")))
-multiplier = 2 #eval(input("Enter wave multiplier: "))
+lowest_tide = 50 #cmToStep(eval(input("Enter height of lowest tide from ground in cm (test = 2): ")))
+tide_range = 300 #cmToStep(eval(input("Enter height between lowest tide and highest tide in cm (test = 30): ")))
+multiplier = 8 #eval(input("Enter wave multiplier: "))
 speed_multiplier = 1.6 #min(eval(input("Enter speed multiplier  (0.8, 1, max = 2): ")),2)
